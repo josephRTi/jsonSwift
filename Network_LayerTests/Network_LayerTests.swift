72ff018f057c655c42aff4183f219570532907d5
josephrt <josephrti@gmail.com>
2021-03-27 09:53:44 +0300

    Initial Commit

---
+++ Network_LayerTests/Network_LayerTests.swift
@@ -0,0 +1,33 @@
+//
+//  Network_LayerTests.swift
+//  Network_LayerTests
+//
+//  Created by Егор Уваров on 27.03.2021.
+//
+
+import XCTest
+@testable import Network_Layer
+
+class Network_LayerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
